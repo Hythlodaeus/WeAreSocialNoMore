9a21de6db9ed41bf49d144689327e9dc3a3aebbc
Hythlodaeus <tangneyjohn@gmail.com>
2015-12-15 11:49:17 +0000

    first push

---
+++ accounts/migrations/0005_auto_20151211_1500.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0004_auto_20151210_1456'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='subscription_end',
+            field=models.DateTimeField(default=datetime.datetime(2015, 12, 11, 15, 0, 36, 849827, tzinfo=utc)),
+        ),
+    ]
